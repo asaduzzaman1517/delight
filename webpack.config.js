8f69585e23d01fe1fbf94adb95ff9f3c33f01c0c
asaduzzaman1517 <asaduzzaman1517@gmail.com>
2018-10-03 16:41:30 +0600

    initial working environment setup complete

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const webpack = require('webpack');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    miniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.(jpg|jpeg|png|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts',
+                            publicPath: '../fonts'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new cleanWebpackPlugin(['dist']),
+        new miniCssExtractPlugin({
+            filename: 'css/[name].css'
+        }),
+        new htmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html'
+        })
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                default: false,
+                vendors: false,
+                vendor: {
+                    chunks: 'all',
+                    test: /node_modules/,
+                    name: 'vendor'
+                }
+            }
+        }
+    }
+};
